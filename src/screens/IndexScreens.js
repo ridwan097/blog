fe9d1c077f247a025063c7591e0802e05a54c6e4
ridwan097 <ridwanishola99@yahoo.com>
2021-08-16 19:46:12 +0100

    more changes

---
+++ src/screens/IndexScreens.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () =>{
+    const {data, addBlogPost} = useContext(BlogContext);
+    
+    return(
+        <View>
+            <Text>IndexScreens page</Text>
+            <Button title="Add post" onPress={addBlogPost} />
+            <FlatList 
+            data={data}
+            keyExtractor={(blogPost) => blogPost.title}
+            renderItem={({item}) =>{
+                return <Text>{item.title}</Text>
+            }}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
