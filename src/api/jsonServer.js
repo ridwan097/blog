b92a758bd4ab4f37805e94e816e916707711ddb1
ridwan097 <ridwanishola99@yahoo.com>
2021-08-21 14:56:00 +0100

    pushing final changes

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://6c15b4897274.ngrok.io'
+})
--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import {Context} from '../context/BlogContext';
 import {Feather} from '@expo/vector-icons';
@@ -6,8 +6,16 @@ import {Feather} from '@expo/vector-icons';
 
 const IndexScreen = ({navigation}) =>{
     
-    const {state, deleteBlogPost} = useContext(Context);
+    const {state, deleteBlogPost, getBlogPosts} = useContext(Context);
     
+
+    useEffect(() =>{
+        getBlogPosts();
+
+        navigation.addListener('didFocus', () =>{
+            getBlogPosts();
+        })
+    }, []);
     return(
         <View>
             <FlatList 
